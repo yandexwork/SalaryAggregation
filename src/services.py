4097e249f47aecb8bbf4de43deaeeab58a679685
Danila Kalganov <dkalganov@team.amocrm.com>
2024-04-22 17:59:02 +0300

    fix: create a new response every time

--- src/services.py
+++ src/services.py
@@ -7,23 +7,24 @@ class SalaryService:
 
     def __init__(self):
         self.database: MongoDatabase = MongoDatabase()
-        self.response: Response = Response()
 
     async def get_salary_information(
             self, _from: str, to: str, group_type:str
     ) -> Response:
 
+        response = Response()
+
         for group_start_date, group_end_date in GroupIterator(_from, to, group_type):
             group_label, total_value = group_start_date.isoformat(), 0
 
-            salaries = self.database.get_salaries(group_end_date, group_end_date)
+            salaries = self.database.get_salaries(group_start_date, group_end_date)
             async for salary in salaries:
                 salary = Salary(**salary)
                 total_value += salary.value
 
-            self.response.add(group_label, total_value)
+            response.add(group_label, total_value)
 
-        return self.response
+        return response
 
 
 salary_service = SalaryService()
